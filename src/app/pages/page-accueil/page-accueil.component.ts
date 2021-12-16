b2a383e2cc117ff64b7048f5a70ff6f754e39a13
Blandine Bajard <83599148+BlandineBajard@users.noreply.github.com>
2021-12-16 10:43:58 +0100

    mise en place de la side-bar catégories

--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import * as _ from 'underscore';
+import { list_products } from '../../data';
 
 @Component({
   selector: 'app-page-accueil',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-accueil.component.scss']
 })
 export class PageAccueilComponent implements OnInit {
+public listData = list_products;
+public listCategoriesFilter!: string[];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    /**
+     * Technique avec underscore js pour récuperer les categories des plantes
+     */
+  const listAllCategories = this.listData.map(product => product.product_breadcrumb_label);
+  console.log(listAllCategories);
+
+  const ListUniqCategories = _.uniq(listAllCategories)  //bibliothèque underscorejs
+  console.log(ListUniqCategories);
+
+  /**
+     * Technique native js v
+     */
+
+  const ListUniqJsCategories = [...new Set(listAllCategories)];
+  console.log(ListUniqJsCategories);
+
+  this.listCategoriesFilter = ListUniqCategories;
+
   }
 
 }
